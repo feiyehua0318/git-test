c0dd1e01994c3eeac9a16dd4c3444205d57cc43d
fyking <fyking0318@gmail.com>
2021-09-21 11:22:01 +0800

    third commit

--- src/main/java/com/fyking/springcloud/HelloSpring.java
+++ src/main/java/com/fyking/springcloud/HelloSpring.java
@@ -7,6 +7,7 @@ public class HelloSpring {
     public static void main(String[] args) {
         System.out.println("你真好");
         System.out.println("第二次测试");
+        System.out.println("第三次提交测试");
     }
 
 }
